f8b1f7143b71990bcf6935476927f10bcf69f4e1
Melan Rashitha Dias <it17006712@my.sliit.lk>
2021-06-20 19:40:17 +0530

    Initial Commit

---
+++ Mexua/MexuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MexuaApp.swift
+//  Mexua
+//
+//  Created by Melan Rashitha Dias on 2021-06-20.
+//
+
+import SwiftUI
+
+@main
+struct MexuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
