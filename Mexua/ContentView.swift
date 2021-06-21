c96931c884ee52e623a88b8a1119009d7c6ad9a3
Melan Rashitha Dias <it17006712@my.sliit.lk>
2021-06-21 07:20:38 +0530

    UI Implementations / SwiftUI

--- Mexua/ContentView.swift
+++ Mexua/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import BottomSheet
 
 struct ContentView: View {
+
+    @State var showMapSetting = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            HStack {
+                Text("Focus /\nRelax / \nBreathe /").font(.largeTitle).bold()
+                Spacer()
+                Text("05").font(.system(size: 70)).bold().font(.largeTitle)
+                
+            }
+            HStack(alignment:.firstTextBaseline , spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                Circle().size(CGSize(width: 10.0, height: 10.0))
+                Circle().size(CGSize(width: 10.0, height: 10.0))
+                Circle().size(CGSize(width: 10.0, height: 10.0))
+            }).fixedSize().frame(width: 400,  alignment: .topLeading)
+            
+            ZStack {
+                Circle().fill(RadialGradient(gradient: Gradient(colors: [Color.yellow, Color.orange,.purple]), center: .top, startRadius: 60, endRadius: 500))
+                
+               
+                
+                Rectangle().frame(width: .infinity, height: 400, alignment: .bottomLeading).foregroundColor(.white).opacity(0.5).blur(radius: 30).position(x: 200, y: 500.0)
+                
+                
+                
+                
+            }
+            HStack {
+                Text("Tap\nCircle to\nStart").font(.title).bold()
+                Spacer()
+                Text("00").font(.system(size:60)).bold()
+            }.padding()
+            
+            
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11")
+                
+                
+            
+        }
     }
 }
---
+++ Mexua/MapSettingView.swift
@@ -0,0 +1,68 @@
+//
+//  MapSettingView.swift
+//  Mexua
+//
+//  Created by Melan Rashitha Dias on 2021-06-20.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct MapSettingView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Map Settings").font(.title).bold()
+                Spacer()
+                Image(systemName: "xmark.circle.fill").foregroundColor(.gray).font(.system(size: 26))
+            }.padding(.horizontal)
+            Picker(selection: .constant(0), label: Text("")) {
+                Text("Map").tag(0)
+                Text("Transit").tag(1)
+                Text("Satellite").tag(2)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal)
+            Divider()
+            Toggle(isOn: .constant(true)) {
+                Text("Traffic").font(.body)
+            }.padding(.horizontal)
+            Rectangle().frame(height: 8).foregroundColor(Color(UIColor.systemGray5))
+            VStack {
+                HStack {
+                    Button(action: { }) {
+                        Text("Mark My Location").font(.system(size: 16))
+                    }
+                    Spacer()
+                }
+                Divider()
+                HStack {
+                    Button(action: { }) {
+                        Text("Add a Missing Place").font(.system(size: 16))
+                    }
+                    Spacer()
+                }
+                Divider()
+                HStack {
+                    Button(action: { }) {
+                        Text("Report an Issue").font(.system(size: 16))
+                    }
+                    Spacer()
+                }
+            }.padding(.horizontal)
+            Rectangle().frame(height: 8).foregroundColor(Color(UIColor.systemGray5))
+            VStack(alignment: .leading) {
+                Text("TomTom").font(.system(size: 14)).bold()
+                Text("© OpenStreetMap and other data providers").font(.caption)
+            }.padding(.horizontal)
+            Spacer()
+        }
+    }
+}
+
+struct MapSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapSettingView()
+    }
+}
